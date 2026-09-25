769cf6c82a082279dfba8a3906fb8f736d8a66f4
unknown <solperaza333@gmail.com>
2026-09-25 10:45:35 -0400

    temporizador visible

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let contador = 20;
-const intervalo = setInterval(() => {
-    contador -=5;
-    console.log(`contador: ${contador}`);
+   // Seleccionamos los elementos del DOM
+        const displayTemporizador = document.getElementById("temporizador");
+        const displayMensaje = document.getElementById("mensaje");
 
-    if (contador === 0) {
-        clearInterval(intervalo);
-        console.log(`temporizador detenido.`);
-    }
-},5000);
+        let contador = 60;
+        
+        // Mostramos el valor inicial de inmediato en pantalla
+        displayTemporizador.textContent = contador;
+
+        const intervalo = setInterval(() => {
+            contador --;
+            
+            // Actualizamos el número en la pantalla
+            displayTemporizador.textContent = contador;
+            console.log(`contador: ${contador}`);
+
+            if (contador === 0) {
+                clearInterval(intervalo);
+                displayMensaje.textContent = "¡Temporizador detenido!";
+                console.log(`temporizador detenido.`);
+            }
+        }, 1000);
